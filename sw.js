d68b57903fc0ffd98622e7fb54e30ad1fa51f7ad
taku_nagai <taky1973@gmail.com>
2018-09-30 09:54:57 +0900

    Initial Commit

---
+++ sw.js
@@ -0,0 +1,22 @@
+// https://developers.google.com/web/fundamentals/primers/service-workers/?hl=ja#https
+
+/**
+ * Service Worker のインストール
+ */
+var CACHE_NAME = 'my-site-cache-v1';
+var urlsToCache = [
+  '/',
+  '/styles/main.css',
+  '/script/main.js'
+];
+
+self.addEventListener('install', function(event) {
+  // インストール処理
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(function(cache) {
+        console.log('Opened cache');
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
